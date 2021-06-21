7b336cebd8258904595b09abbb2bf231aa20fc05
29741662 <ntthanh3@cmc.com.vn>
2021-06-21 22:46:59 +0700

    add helper

---
+++ Base.Data/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Data.Models
+{
+    [Table("User")]
+    public class User:BaseClass
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string EmailAddress { get; set;}
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+    }
+}
